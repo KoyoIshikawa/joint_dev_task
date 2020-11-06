96361e266454e51bf824037bb3a1be027e971810
KoyoIshikawa <koyoi561@gmail.com>
2020-11-06 14:36:34 +0900

    Q9 Q10 Q11 の解答を追加

--- task.rb
+++ task.rb
@@ -80,7 +80,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+    names.each.with_index(1) do |name,i|
+      puts "会員No.#{i} #{name}さん"
+    end
 end
 
 def q10
@@ -88,13 +90,26 @@ def q10
 
   # 以下に回答を記載
 
+  p foods
+  foods.each do |food|
+    if food.include?("うに")
+      puts "#{food}は好物です"
+    else
+      puts "#{food}はまぁまぁ好きです"
+    end   
+  end
+
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  # sports.uniq!(&:first)
+  flatten_sports = sports.flatten.uniq!
+  flatten_sports.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+  end
 end
 
 def q12
