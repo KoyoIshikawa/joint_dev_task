4c469cb3ae924bf5705a83b321f870cfa78359a9
KoyoIshikawa <koyoi561@gmail.com>
2020-11-20 23:39:53 +0900

    Q18,Q19の回答を追加

--- task.rb
+++ task.rb
@@ -206,7 +206,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age]
+  end
 
+  def introduce
+    if @age >= 30
+      "こんにちは，#{@name}と申します。宜しくお願いいたします。"
+    else
+      "はいさいまいど〜，#{@name}です！！！"
+    end
+  end
 end
 
 def q18
@@ -220,8 +231,8 @@ end
 
 class Item
   # 以下を修正して下さい
-
-  def initialize(name)
+  attr_reader :name
+  def initialize(name:)
     @name = name
   end
 end
