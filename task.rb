ef70e23c8e61c47761176c407fb6097c63d69284
KoyoIshikawa <koyoi561@gmail.com>
2020-11-21 16:32:19 +0900

    if→caseに変更、attr_accesor→attr_readerに変更

--- task.rb
+++ task.rb
@@ -245,7 +245,7 @@ end
 
 class UserQ20
   # 以下に回答を記載
-    attr_accessor :name, :age
+    attr_reader :name, :age
   def initialize(**params)
     @name = params[:name]
     @age = params[:age]
@@ -255,26 +255,23 @@ end
 
 class Zoo
   # 以下に回答を記載
-  attr_accessor :name, :entry_fee
   def initialize(**params)
     @name = params[:name]
     @entry_fee = params[:entry_fee]
   end
   def info_entry_fee(user)
-
-    if user.age <= 5 and user.age >=0
-      puts "#{user.name}の入場料金は#{@entry_fee[:infant]}円です。"
-    
-    elsif user.age <= 12 and user.age >=6
-      puts "#{user.name}の入場料金は#{@entry_fee[:children]}円です。"
-    
-    elsif user.age <= 64 and user.age >=13
-      puts "#{user.name}の入場料金は#{@entry_fee[:adult]}円です。"
-    
-    elsif user.age <= 120 and user.age >=65
-      puts "#{user.name}の入場料金は#{@entry_fee[:senior]}円です。"
-
+    case user.age
+      when 0..5then
+        puts "#{user.name}の入場料金は#{@entry_fee[:infant]}円です。"
       
+      when 6..12 then
+        puts "#{user.name}の入場料金は#{@entry_fee[:children]}円です。"
+      
+      when 13..64 then
+        puts "#{user.name}の入場料金は#{@entry_fee[:adult]}円です。"
+      
+      when 65..120 then
+        puts "#{user.name}の入場料金は#{@entry_fee[:senior]}円です。"
     end
   end
 
